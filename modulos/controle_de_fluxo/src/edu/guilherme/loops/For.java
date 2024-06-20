c0eda151295a9a46a04f541b17021bbcb22bf700
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-06-20 12:14:23 -0300

    "for" loop

---
+++ modulos/controle_de_fluxo/src/edu/guilherme/loops/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+        for (int sheeps = 1; sheeps <= 20; sheeps++) {
+            String message = sheeps == 1 ? " sheep" : " sheeps";
+            System.out.println(sheeps + message);
+        }
+    }
+}
